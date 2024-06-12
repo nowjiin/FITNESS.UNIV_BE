94902809bed7ebe992b6e2d8ce1fe9fd294760cb
nowjin <hyunjin117@naver.com>
2024-06-12 19:25:33 +0900

    ✨Feat: AccessToken 만료 됬을 경우 재발급 로직 추가

--- src/main/java/com/hecto/fitnessuniv/controller/AuthController.java
+++ src/main/java/com/hecto/fitnessuniv/controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.hecto.fitnessuniv.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.hecto.fitnessuniv.entity.UserEntity;
 import com.hecto.fitnessuniv.provider.JwtProvider;
+import com.hecto.fitnessuniv.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,24 +17,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
     private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
 
     @PostMapping("/refresh-token")
     public ResponseEntity<?> refreshAuthToken(@RequestHeader("Authorization") String bearerToken) {
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            try {
-                String token = bearerToken.substring(7);
-                if (jwtProvider.validate(token)) {
-//                    String newToken = jwtProvider.refreshToken(token);
-//                    return ResponseEntity.ok(Map.of("token", newToken));
+            String refreshToken = bearerToken.substring(7);
+            if (jwtProvider.validate(refreshToken)) {
+                String userId = jwtProvider.getUserIdFromToken(refreshToken);
+                Optional<UserEntity> userOptional = userRepository.findById(userId);
+                if (userOptional.isPresent()) {
+                    UserEntity user = userOptional.get();
+                    if (refreshToken.equals(user.getRefreshToken())) {
+                        String newAccessToken = jwtProvider.refreshAccessToken(refreshToken);
+                        return ResponseEntity.ok(Map.of("token", newAccessToken));
+                    } else {
+                        return ResponseEntity.status(401).body("Invalid refresh token");
+                    }
                 } else {
-                    return ResponseEntity.status(401).body("Invalid token");
+                    return ResponseEntity.status(404).body("User not found");
                 }
-            } catch (Exception e) {
-                return ResponseEntity.status(500).body("Token refresh failed");
+            } else {
+                return ResponseEntity.status(401).body("Invalid token");
             }
         } else {
             return ResponseEntity.status(400).body("Bearer token is missing or invalid");
         }
-        return null;
     }
 }
