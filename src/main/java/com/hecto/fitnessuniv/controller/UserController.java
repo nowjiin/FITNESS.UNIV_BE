ea61de6d5bdc2bb641f50fd1a6d9f3a13c6c7775
nowjin <hyunjin117@naver.com>
2024-06-13 21:54:20 +0900

    ✨Feat: Mentor Profile 조회 API 추가

--- src/main/java/com/hecto/fitnessuniv/controller/UserController.java
+++ src/main/java/com/hecto/fitnessuniv/controller/UserController.java
@@ -2,10 +2,15 @@ package com.hecto.fitnessuniv.controller;
 
 import java.util.Optional;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.hecto.fitnessuniv.entity.UserEntity;
+import com.hecto.fitnessuniv.provider.JwtProvider;
+import com.hecto.fitnessuniv.repository.MenteeProfileRepository;
+import com.hecto.fitnessuniv.repository.MentorProfileRepository;
 import com.hecto.fitnessuniv.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,15 +20,46 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+    private final MentorProfileRepository mentorProfileRepository;
+    private final MenteeProfileRepository menteeProfileRepository;
+    private final JwtProvider jwtProvider;
 
     @GetMapping("/check-user-role")
-    public ResponseEntity<String> checkUserExists(@RequestParam String userId) {
-        Optional<UserEntity> userEntityOptional = userRepository.findById(userId);
-        if (userEntityOptional.isPresent()) {
-            UserEntity userEntity = userEntityOptional.get();
-            return ResponseEntity.ok(userEntity.getUserRole());
+    public ResponseEntity<String> checkUserRole(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            String token = bearerToken.substring(7);
+            if (jwtProvider.validate(token)) {
+                String userId = jwtProvider.getUserIdFromToken(token);
+                Optional<UserEntity> userEntityOptional = userRepository.findByUserId(userId);
+                if (userEntityOptional.isPresent()) {
+                    UserEntity userEntity = userEntityOptional.get();
+                    return ResponseEntity.ok(userEntity.getRole());
+                } else {
+                    return ResponseEntity.ok("ROLE_NEW");
+                }
+            } else {
+                return ResponseEntity.status(401).body("Invalid token");
+            }
         } else {
-            return ResponseEntity.ok("ROLE_NEW");
+            return ResponseEntity.status(400).body("Bearer token is missing or invalid");
+        }
+    }
+
+    @GetMapping("/get-user-id")
+    public ResponseEntity<String> getUserId(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            String token = bearerToken.substring(7);
+            if (jwtProvider.validate(token)) {
+                String userId = jwtProvider.getUserIdFromToken(token);
+                System.out.println("Extracted User ID: " + userId);
+                return ResponseEntity.ok(userId);
+            } else {
+                return ResponseEntity.status(401).body("Invalid token");
+            }
+        } else {
+            return ResponseEntity.status(400).body("Bearer token is missing or invalid");
         }
     }
 }
